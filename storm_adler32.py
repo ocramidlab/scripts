da6ffc622bf55974a914b747bcec419332bbe06b
Marco Baldi <marco.baldi5@Str957-cluster.personale.dir.unibo.it>
2019-05-06 11:30:28 +0200

    adding storm adler

---
+++ storm_adler32.py
@@ -0,0 +1,16 @@
+
+import sys
+from zlib import adler32
+BLOCKSIZE=256*1024*1024
+
+for fname in sys.argv[1:]:
+ asum = 1
+ with open(fname) as f:
+   while True:
+     data = f.read(BLOCKSIZE)
+     if not data:
+        break
+     asum = adler32(data, asum)
+     if asum < 0:
+       asum += 2**32
+print hex(asum)[2:10].zfill(8).lower(), fname
