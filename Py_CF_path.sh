705bf89d44507e4ea03a48b3f9aad28e4f0af660
Marco Baldi <mbaldi00@r000u07l02.marconi.cineca.it>
2019-09-03 08:48:05 +0200

    adding correlation function

---
+++ Py_CF_path.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+#Usage: Py_CF_path.py output_Path Snapnum_min Snapnum_max BoxSize GridSize particle_types[numbers separated by comma]
+NUMPARAMS=6 # The number of expected parameters in the script
+
+#SCRIPTSPATH='/home/PERSONALE/marco.baldi5/SOURCE/python_scripts/'
+#BASE='/marconi_scratch/userexternal/mbaldi00/PNG/B4000_1024_fNL+40/output/'
+
+if [ $# != $NUMPARAMS ]; then
+    echo "Usage: Py_Pk_path.py output_path Snapnum_min Snapnum_max BoxSize GridSize particle_types[numbers separated by comma]"
+    exit 0
+else
+    BASE=$1
+    for SNAPNUM in $(eval echo {$2..$3})
+    do
+	NUM=$(printf %03d $SNAPNUM)
+	TARGET=$BASE'/snapdir_'$NUM
+	echo ${TARGET}
+ 	if [ -d ${TARGET} ]; then
+	    echo $TARGET
+	    echo Copying python script to local
+	    cp ${SCRIPTSPATH}quick_CF.py $TARGET
+	    echo Done
+	    echo Moving to $TARGET
+	    cd $TARGET
+	    echo Done
+	    SNAPNAME=$(ls -1 *.0)
+	    echo we use this: ${SNAPNAME%.*} as snapshot argument
+	    echo Running the python script
+	    python quick_CF.py ${SNAPNAME%.*} $4 $5 $6
+	    echo Done
+	    rm ./quick_CF.py
+	fi
+	    echo Correlation Function Computed
+    done
+fi
+
